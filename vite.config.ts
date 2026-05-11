f9ba64ec357801fbe449d46873929071e098e11c
HEM-77 <167549643+HEM-77@users.noreply.github.com>
2026-05-11 19:20:30 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-base: "/rubin-sustainability-links/"
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 })
